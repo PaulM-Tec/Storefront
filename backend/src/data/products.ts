3dc1689ddfb6fd272c063dcc278f94d69a7b89bc
Paul Munhamo <pmunham@outlook.com>
2025-12-06 13:41:30 +0200

    Added another product

--- backend/src/data/products.ts
+++ backend/src/data/products.ts
@@ -12,20 +12,20 @@ export type Product = {
 export const products: Product[] = [
   {
     id: 'p-001',
-    name: 'MacBook Air',
+    name: 'Apple MacBook Air 13-inch',
     description: 'Thin, lightweight design, silent fanless operation, long battery life',
     price: 11499.0,
     imageUrl: 'https://picsum.photos/id/180/600/400',
     category: 'Laptop',
-    stock: 16,
+    stock: 5,
   },
   {
     id: 'p-002',
-    name: 'Mechanical Keyboard',
-    description: 'Hot-swappable, RGB',
-    price: 1299.0,
-    imageUrl: 'https://picsum.photos/id/1080/600/400',
-    category: 'Peripherals',
-    stock: 24,
+    name: 'Apple MacBook Air 15-inch',
+    description: 'The ultimate on-the-go laptop, giving you more space onscreen for multi­tasking',
+    price: 18299.0,
+    imageUrl: 'https://picsum.photos/id/6/5000/3333.jpg?hmac=pq9FRpg2xkAQ7J9JTrBtyFcp9-qvlu8ycAi7bUHlL7I',
+    category: 'Laptops',
+    stock: 7,
   },
 ];
